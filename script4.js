7abd8e0407d3783a0e7a6834d404916502dc7f4b
kapeiii <138109641+kapeiii@users.noreply.github.com>
2023-07-31 10:53:37 +0800

    script4.js

--- script4.js
+++ script4.js
@@ -19,7 +19,7 @@ const func = () => {
 
     let gwa = total.toPrecision(5);
 
-    if(gwa<=1.75 && g1<=2.5 && g2<=2.5 && g3<=2.5 && g4<=2.5){
+    if(gwa<=1.75 && g1<2.5 && g2<2.5 && g3<2.5 && g4<2.5){
         document.getElementById('showData-1').innerHTML = ""+ gwa;
         document.getElementById('showData-2').innerHTML = `<h3 style="color:white; font-size:15px">You're a <strong style="color:orange;">DEAN LISTER.</strong> Congratulations!</h3>`
     } else if (g1<=0 || g1>5 || g2<=0 || g2>5 || g3<=0 || g3>5 || g4<=0 || g4>5 || u1<=0 || u2<=0 || u3<=0 || u4<=0){
