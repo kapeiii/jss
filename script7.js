12259d5a95f31ee19d261b845fc539e03dbc97c6
kapeiii <138109641+kapeiii@users.noreply.github.com>
2023-07-31 17:55:03 +0800

    script7.js

--- script7.js
+++ script7.js
@@ -31,11 +31,11 @@ const func = () => {
     if(gwa<=1.75 && g1<2.5 && g2<2.5 && g3<2.5 && g4<2.5 && g5<2.5 && g6<2.5 && g7<2.5){
         document.getElementById('showData-1').innerHTML = ""+ gwa;
         document.getElementById('showData-2').innerHTML = `<h3 style="color:white; font-size:15px">You're a <strong style="color:orange;">DEAN LISTER.</strong> Congratulations!</h3>`
-    } else if (g1<=0 || g1>5 || g2<=0 || g2>5 || g3<=0 || g3>5 || g4<=0 || g4>5 || g5<=0 || g5>5 || g6<=0 || g6>5 || g7<=0 || g7>5 || u1<=0 || u2<=0 || u3<=0 || u4<=0 || u5<=0 || u6<=0 || u7<=0){
-        document.getElementById('showData-1').innerHTML = `<h1 style="color:red; font-size:18px">ERROR</h1>`
-        document.getElementById('showData-2').innerHTML = `Something went wrong. Please make sure that you fill out all of the needed information.`
-    } else {
+    } else if (gwa<=5 && g1>0 && g1<=5 && g2>0 && g2<=5 && g3>0 && g3<=5 && g4>0 && g4<=5 && g5>0 && g5<=5 && g6>0 && g6<=5 && g7>0 && g7<=5 && u1>0 && u2>0 && u3>0 && u4>0 && u5>0 && u6>0 && u7>0){
         document.getElementById('showData-1').innerHTML = ""+ gwa;
         document.getElementById('showData-2').innerHTML = `<h3 style="color:white; font-size:15px">Nice work. Keep it up!</h3>`
+    } else {
+        document.getElementById('showData-1').innerHTML = `<h1 style="color:red; font-size:18px">ERROR</h1>`
+        document.getElementById('showData-2').innerHTML = `Something went wrong. Please make sure that you fill out all of the needed information.`
     }
 }
