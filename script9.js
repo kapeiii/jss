fbe3ac54cc7ce4538c7fc78dcc73b89fba48e30f
kapeiii <138109641+kapeiii@users.noreply.github.com>
2023-07-31 10:58:23 +0800

    script9.js

--- script9.js
+++ script9.js
@@ -34,7 +34,7 @@ const func = () => {
 
     let gwa = total.toPrecision(5);
 
-    if(gwa<=1.75 && g1<=2.5 && g2<=2.5 && g3<=2.5 && g4<=2.5 && g5<=2.5 && g6<=2.5 && g7<=2.5 && g8<=2.5 && g9<=2.5){
+    if(gwa<=1.75 && g1<2.5 && g2<2.5 && g3<2.5 && g4<2.5 && g5<2.5 && g6<2.5 && g7<2.5 && g8<2.5 && g9<2.5){
         document.getElementById('showData-1').innerHTML = ""+ gwa;
         document.getElementById('showData-2').innerHTML = `<h3 style="color:white; font-size:15px">You're a <strong style="color:orange;">DEAN LISTER.</strong> Congratulations!</h3>`
     } else if (g1<=0 || g1>5 || g2<=0 || g2>5 || g3<=0 || g3>5 || g4<=0 || g4>5 || g5<=0 || g5>5 || g6<=0 || g6>5 || g7<=0 || g7>5 || g8<=0 || g8>5 || g9<=0 || g9>5 || u1<=0 || u2<=0 || u3<=0 || u4<=0 || u5<=0 || u6<=0 || u7<=0 || u8<=0 || u9<=0){
