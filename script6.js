5fe7e5afebb7143c37de567e35459e3372aece6c
kapeiii <138109641+kapeiii@users.noreply.github.com>
2024-01-14 07:51:14 +0800

    script6.js

--- script6.js
+++ script6.js
@@ -34,9 +34,9 @@ const func = () => {
         document.getElementById('showData-2').innerHTML = `<h3 style="color:white; font-size:15px">You're a <strong style="color:orange;">DEAN'S LISTER.</strong> Congratulations!</h3>`
     } else if (gwa<=5 && g1>0 && g1<=5 && g2>0 && g2<=5 && g3>0 && g3<=5 && g4>0 && g4<=5 && g5>0 && g5<=5 && g6>0 && g6<=5 && u1>0 && u2>0 && u3>0 && u4>0 && u5>0 && u6>0){
         document.getElementById('showData-1').innerHTML = ""+ gwa;
-        document.getElementById('showData-2').innerHTML = `<h3 style="color:white; font-size:15px">Nice work. Keep it up!</h3>`
+        document.getElementById('showData-2').innerHTML = `<h3 style="color:white; font-size:15px">Ginawa ko naman lahat, hindi pa rin sapat</h3>`
     } else {
         document.getElementById('showData-1').innerHTML = `<h1 style="color:red; font-size:18px">ERROR</h1>`
-        document.getElementById('showData-2').innerHTML = `Something went wrong. Please Try again.`
+        document.getElementById('showData-2').innerHTML = `Wala ka na ginawang tama`
     }
 }
