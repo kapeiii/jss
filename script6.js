6b51561494bce585e397ba53e076b708c305bc9b
kapeiii <138109641+kapeiii@users.noreply.github.com>
2024-06-18 16:44:33 +0800

    Update script6.js

--- script6.js
+++ script6.js
@@ -1,4 +1,5 @@
 const func = () => {
+    let name = document.getElementById('name').value;
     let g1 = document.getElementById('1st').value;
     let g2 = document.getElementById('2nd').value;
     let g3 = document.getElementById('3rd').value;
@@ -26,17 +27,21 @@ const func = () => {
     let gwa = total.toPrecision(5);
 
     if(gwa<=1.45 && g1<=2.4 && g2<=2.4 && g3<=2.4 && g4<=2.4 && g5<=2.4 && g6<=2.4 && u1>0 && u2>0 && u3>0 && u4>0 && u5>0 && u6>0){
-        document.getElementById('showData-1').innerHTML = ""+ gwa;
-        document.getElementById('showData-2').innerHTML = `<h3 style="color:white; font-size:15px">You're a <strong style="color:yellow;">PRESIDENT'S LISTER.</strong> Excellent!</h3>`
+        document.getElementById('showData-2').innerHTML = name;
+        document.getElementById('showData-3').innerHTML = gwa;
+        document.getElementById('showData-1').innerHTML = `<strong style="color:yellow;"> PRESIDENT'S LISTER </strong>`
     }
     else if(gwa<=1.75 && g1<=2.4 && g2<=2.4 && g3<=2.4 && g4<=2.4 && g5<=2.4 && g6<=2.4 && u1>0 && u2>0 && u3>0 && u4>0 && u5>0 && u6>0){
-        document.getElementById('showData-1').innerHTML = ""+ gwa;
-        document.getElementById('showData-2').innerHTML = `<h3 style="color:white; font-size:15px">You're a <strong style="color:orange;">DEAN'S LISTER.</strong> Great job!</h3>`
+        document.getElementById('showData-2').innerHTML = name;
+        document.getElementById('showData-3').innerHTML = gwa;
+        document.getElementById('showData-1').innerHTML = `<strong style="color:orange;"> DEAN'S LISTER </strong>`
     } else if (gwa<=5 && g1>0 && g1<=5 && g2>0 && g2<=5 && g3>0 && g3<=5 && g4>0 && g4<=5 && g5>0 && g5<=5 && g6>0 && g6<=5 && u1>0 && u2>0 && u3>0 && u4>0 && u5>0 && u6>0){
-        document.getElementById('showData-1').innerHTML = ""+ gwa;
-        document.getElementById('showData-2').innerHTML = `<h3 style="color:white; font-size:15px">Just a little more, you’ve got this!</h3>`
+        document.getElementById('showData-2').innerHTML = name;
+        document.getElementById('showData-3').innerHTML = gwa;
+        document.getElementById('showData-1').innerHTML = `<h3 style="color:white; font-size:15px">Just a little more, you’ve got this!</h3>`
     } else {
         document.getElementById('showData-1').innerHTML = `<h1 style="color:red; font-size:18px">ERROR</h1>`
         document.getElementById('showData-2').innerHTML = `Hmm, that didn't work. Try again?`
+        document.getElementById('showData-3').innerHTML = "";
     }
 }
