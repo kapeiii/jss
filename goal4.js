c61d8375294cfe8c7a629f46680a1b71d10000b9
kapeiii <138109641+kapeiii@users.noreply.github.com>
2024-07-03 07:15:33 +0800

    Add files via upload

---
+++ goal4.js
@@ -0,0 +1,135 @@
+const func = () => {
+    let g1 = document.getElementById('1st').value;
+    let g2 = document.getElementById('2nd').value;
+    let g3 = document.getElementById('3rd').value;
+    let g4 = document.getElementById('4th').value;
+    let u1 = document.getElementById('1st-2').value;
+    let u2 = document.getElementById('2nd-2').value;
+    let u3 = document.getElementById('3rd-2').value;
+    let u4 = document.getElementById('4th-2').value;
+    let remaining = document.getElementById('us').value;
+
+    let avg1 = parseFloat(g1) * parseFloat(u1);
+    let avg2 = parseFloat(g2) * parseFloat(u2);
+    let avg3 = parseFloat(g3) * parseFloat(u3);
+    let avg4 = parseFloat(g4) * parseFloat(u4);
+
+    let unit = parseFloat(u1)+parseFloat(u2)+parseFloat(u3)+parseFloat(u4);
+
+    let total = (avg1 + avg2 + avg3 + avg4)/unit;
+
+    let gwa = total.toPrecision(5);
+
+    let tAverage = avg1 + avg2 + avg3 + avg4;
+    let tUnitsRequired = unit + parseFloat(remaining);
+    
+    let probabilityPL = (1.45 * tUnitsRequired) - tAverage;
+    let pl = probabilityPL/remaining;
+    let finalPL = pl.toPrecision(5);
+
+    let probabilityDL = (1.75 * tUnitsRequired) - tAverage;
+    let dl = probabilityDL/remaining;
+    let finalDL = dl.toPrecision(5);
+
+    if (finalDL<1 && g1<=2.4 && g1>=1 && g2<=2.4 && g2>=1 && g3<=2.4 && g3>=1 && g4<=2.4 && g4>=1 && u1>0 && u2>0 && u3>0 && u4>0 && remaining>=1){
+        document.getElementById('showData-1').innerHTML = `<strong style="color:yellow;"> CURRENT GWA </strong>`
+        document.getElementById('showData-2').innerHTML = "";
+        document.getElementById('showData-3').innerHTML = gwa;
+        document.getElementById('showData-4').innerHTML = `<strong style="color:yellow;"> President Lister Goal </strong>`
+        document.getElementById('showData-5').innerHTML = "";
+        document.getElementById('showData-6').innerHTML = `impossible`;
+        document.getElementById('showData-7').innerHTML = `<strong style="color:yellow;"> Dean Lister Goal </strong>`
+        document.getElementById('showData-8').innerHTML = "";
+        document.getElementById('showData-9').innerHTML = `impossible`;
+        document.getElementById('showData-10').innerHTML = `------------------`;
+    }
+    else if (finalPL>=2.4 && g1>=1 && g2<=2.4 && g2>=1 && g3<=2.4 && g3>=1 && g4<=2.4 && g4>=1 && u1>0 && u2>0 && u3>0 && u4>0 && remaining>=1){
+        document.getElementById('showData-1').innerHTML = `<strong style="color:yellow;"> CURRENT GWA </strong>`
+        document.getElementById('showData-2').innerHTML = "";
+        document.getElementById('showData-3').innerHTML = gwa;
+        document.getElementById('showData-4').innerHTML = `<strong style="color:yellow;"> President Lister Goal </strong>`
+        document.getElementById('showData-5').innerHTML = `SECURED`;
+        document.getElementById('showData-6').innerHTML = `(maintain remaining subject/s above 2.5)`;
+        document.getElementById('showData-7').innerHTML = "";
+        document.getElementById('showData-8').innerHTML = "";
+        document.getElementById('showData-9').innerHTML = "";
+        document.getElementById('showData-10').innerHTML = `------------------`;
+    }
+    else if (finalDL<=2.4 && finalPL<1 && g1<=2.4 && g1>=1 && g2<=2.4 && g2>=1 && g3<=2.4 && g3>=1 && g4<=2.4 && g4>=1 && u1>0 && u2>0 && u3>0 && u4>0 && remaining>=1){
+        document.getElementById('showData-1').innerHTML = `<strong style="color:yellow;"> CURRENT GWA </strong>`
+        document.getElementById('showData-2').innerHTML = "";
+        document.getElementById('showData-3').innerHTML = gwa;
+        document.getElementById('showData-4').innerHTML = `<strong style="color:yellow;"> President Lister Goal </strong>`
+        document.getElementById('showData-5').innerHTML = "";
+        document.getElementById('showData-6').innerHTML = `impossible`;
+        document.getElementById('showData-7').innerHTML = `<strong style="color:yellow;"> Dean Lister Goal </strong>`
+        document.getElementById('showData-8').innerHTML = finalDL;
+        document.getElementById('showData-9').innerHTML = `(minimum grade for each remaining subjects)`;
+        document.getElementById('showData-10').innerHTML = `------------------`;
+    }
+    else if (finalPL<1 && finalDL>2.4 && g1<=2.4 && g1>=1 && g2<=2.4 && g2>=1 && g3<=2.4 && g3>=1 && g4<=2.4 && g4>=1 && u1>0 && u2>0 && u3>0 && u4>0 && remaining>=1){
+        document.getElementById('showData-1').innerHTML = `<strong style="color:yellow;"> CURRENT GWA </strong>`
+        document.getElementById('showData-2').innerHTML = "";
+        document.getElementById('showData-3').innerHTML = gwa;
+        document.getElementById('showData-4').innerHTML = `<strong style="color:yellow;"> President Lister Goal </strong>`
+        document.getElementById('showData-5').innerHTML = "";
+        document.getElementById('showData-6').innerHTML = `impossible`;
+        document.getElementById('showData-7').innerHTML = `<strong style="color:yellow;"> Dean Lister Goal </strong>`
+        document.getElementById('showData-8').innerHTML = `SECURED`;
+        document.getElementById('showData-9').innerHTML = `(maintain remaining subject/s above 2.5)`;
+        document.getElementById('showData-10').innerHTML = `------------------`;
+    }
+    else if (finalDL<2.4 && finalPL<2.4 && g1<=2.4 && g1>=1 && g2<=2.4 && g2>=1 && g3<=2.4 && g3>=1 && g4<=2.4 && g4>=1 && u1>0 && u2>0 && u3>0 && u4>0 && remaining>=1){
+        document.getElementById('showData-1').innerHTML = `<strong style="color:yellow;"> CURRENT GWA </strong>`
+        document.getElementById('showData-2').innerHTML = "";
+        document.getElementById('showData-3').innerHTML = gwa;
+        document.getElementById('showData-4').innerHTML = `<strong style="color:yellow;"> President Lister Goal </strong>`
+        document.getElementById('showData-5').innerHTML = finalPL;
+        document.getElementById('showData-6').innerHTML = `(minimum grade for each remaining subjects)`;
+        document.getElementById('showData-7').innerHTML = `<strong style="color:yellow;"> Dean Lister Goal </strong>`
+        document.getElementById('showData-8').innerHTML = finalDL;
+        document.getElementById('showData-9').innerHTML = `(minimum grade for each remaining subjects)`;
+        document.getElementById('showData-10').innerHTML = `------------------`;
+    }
+    else if (finalPL<2.4 && finalDL>=2.4 && g1<=2.4 && g1>=1 && g2<=2.4 && g2>=1 && g3<=2.4 && g3>=1 && g4<=2.4 && g4>=1 && u1>0 && u2>0 && u3>0 && u4>0 && remaining>=1){
+        document.getElementById('showData-1').innerHTML = `<strong style="color:yellow;"> CURRENT GWA </strong>`
+        document.getElementById('showData-2').innerHTML = "";
+        document.getElementById('showData-3').innerHTML = gwa;
+        document.getElementById('showData-4').innerHTML = `<strong style="color:yellow;"> President Lister Goal </strong>`
+        document.getElementById('showData-5').innerHTML = finalPL;
+        document.getElementById('showData-6').innerHTML = `(minimum grade for each remaining subjects)`;
+        document.getElementById('showData-7').innerHTML = `<strong style="color:yellow;"> Dean Lister Goal </strong>`
+        document.getElementById('showData-8').innerHTML = `SECURED`;
+        document.getElementById('showData-9').innerHTML = `(maintain remaining subject/s above 2.5)`;
+        document.getElementById('showData-10').innerHTML = `------------------`;
+    }
+    else if (g1>=1 && g1<5 && g2>=1 && g2<5 && g3>=1 && g3<5 && g4>=1 && g4<5 && u1>0 && u2>0 && u3>0 && u4>0 && remaining>=1){
+        document.getElementById('showData-1').innerHTML = `<strong style="color:yellow;"> CURRENT GWA </strong>`
+        document.getElementById('showData-2').innerHTML = "";
+        document.getElementById('showData-3').innerHTML = gwa;
+        document.getElementById('showData-4').innerHTML = `<strong style="color:yellow;"> President Lister Goal </strong>`
+        document.getElementById('showData-5').innerHTML = "";
+        document.getElementById('showData-6').innerHTML = `impossible`;
+        document.getElementById('showData-7').innerHTML = `<strong style="color:yellow;"> Dean Lister Goal </strong>`
+        document.getElementById('showData-8').innerHTML = "";
+        document.getElementById('showData-9').innerHTML = `impossible`;
+        document.getElementById('showData-10').innerHTML = `------------------`;
+    }
+    else {
+        document.getElementById('showData-1').innerHTML = `<h1 style="color:red; font-size:18px">ERROR</h1>`
+        document.getElementById('showData-2').innerHTML = `Hmm, that didn't work. Try again?`
+        document.getElementById('showData-3').innerHTML = "";
+        document.getElementById('showData-4').innerHTML = "";
+        document.getElementById('showData-5').innerHTML = "";
+        document.getElementById('showData-6').innerHTML = "";
+        document.getElementById('showData-7').innerHTML = "";
+        document.getElementById('showData-8').innerHTML = "";
+        document.getElementById('showData-9').innerHTML = "";
+        document.getElementById('showData-10').innerHTML = "";
+    }
+    
+
+    
+
+
+}
